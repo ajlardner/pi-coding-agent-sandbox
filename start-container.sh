632ddeaa3986877cccf3ae25e94e999139735297
Aidan Lardner <aj@ajlardner.net>
2026-08-21 20:27:58 -0400

    revert setting agent dir to ro

--- start-container.sh
+++ start-container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-PROJECTS_DIR=${PROJECTS_DIR:-$HOME/projects}
-PI_ENV_FILE=${PI_ENV_FILE:-$PROJECTS_DIR/pi-sandbox/.env}
+PI_ENV_FILE=${PI_ENV_FILE:-$HOME/pi-sandbox/.env}
 PI_WORKSPACE=${PI_WORKSPACE:-$PWD}
 PI_CONFIG_DIR=${PI_CONFIG_DIR:-$HOME/.pi}
 
 # TODO - dont let this be run in the same directory as the script
 
-podman run --rm -it --network host --env-file "$PI_ENV_FILE" -v $PI_WORKSPACE:/workspace -v $PI_CONFIG_DIR:/root/.pi pi-coding-agent
+podman run --rm -it --network host --env-file "$PI_ENV_FILE" -v $PI_WORKSPACE:/workspace:ro -v $PI_CONFIG_DIR:/root/.pi pi-coding-agent
